42030e8dfb435f1e1cd8bd1896100703e933207f
ellardaaron <59664272+ellardaaron@users.noreply.github.com>
2020-02-03 18:41:43 -0600

    Update app.js
    changed list items to buttons and adjusted numbering

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var friends = ['Katherine', 'Ryan', 'Standing Ryan', 'Jillian'];
 var locations = ['Kitchen', 'Laundry Room', 'Living Room', 'Dinning Hall'];
 var count = 100
 
-for (var i = 0; i < 101; i++) {
+for (var i = 1; i < 101; i++) {
     var accusationTxt = document.createElement ('h3','ul');
-    var listItem = document.createElement ('li');
+    var listItem = document.createElement ('button');
     var text = document.createTextNode ('accusation ' + i);
     listItem.appendChild(text);
     accusationTxt.appendChild(listItem);
